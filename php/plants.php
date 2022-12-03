00c30125b30ea32c359fba922f0393a3942f242a
Jeremias <jer.greis.21@lehre.mosbach.dhbw.de>
2022-12-03 15:27:16 +0100

    TEST

--- php/plants.php
+++ php/plants.php
@@ -14,6 +14,7 @@ $_SESSION['home_page']="../index.php";
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    TEST Jeremias
 </head>
 <body>
 <?php include __DIR__.'/templates/header.php'; ?>
