7a718d0fa9cc5e5b9e7d7b9a11179075f73093ed
TEC\BSC <bernhard.schwab@tecalliance.net>
2022-12-05 09:41:36 +0100

    Habe Favicon hinzugefügt ( Symbol, dass oben im Tab zu sehen ist)

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $_SESSION['signin_page']="php/signin.php";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OnlyPlants</title>
-    <!--<link rel="icon" href="favicon.png">-->
+    <link rel="icon" href="favicon.png">
     <!--This link has to be in the head for bootstrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
