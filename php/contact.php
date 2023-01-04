aa75d7353d7901b350d236318195e6b0340e426a
vicky-dhbw <101749450+vicky-dhbw@users.noreply.github.com>
2023-01-04 15:19:43 +0100

    contact animation

--- php/contact.php
+++ php/contact.php
@@ -37,60 +37,83 @@ $number=0;
 
 <style>
     .form-container {
-        margin: 170px;
-        padding: 20px;
         border: 1px solid #ccc;
     }
+
+    .myCont-{
+        margin-top: 170px;
+        padding-bottom: 40px;
+    }
+    .anim{
+        width: 20vh;
+        height: auto;
+    }
 </style>
-<div class="form-container">
-    <h1>Contact Page</h1>
-    <form action="" method="post">
-        <p>Please read our <a href="privacyPolicy.php">privacy policy</a> before filling out the form.</p>
-        <div class="form-group mb-4">
-            <label for="name">Name:</label>
-            <input type="text" class="form-control my-2" id="name" name="name" required>
-        </div>
-        <div class="form-group mb-4">
-            <label for="email">Email address:</label>
-            <input type="email" class="form-control my-2" id="email" name="email" required>
-        </div>
-        <div class="form-group mb-4">
-            <label class="mb-2" for="message">Message:</label>
-            <textarea class="form-control" id="message" name="message" required></textarea>
-        </div>
-        <div class="form-group mb-4">
-            <input type="checkbox" id="privacy-policy" name="privacy-policy" required>
-            <label for="privacy-policy">I have read the <a href="privacyPolicy.php">privacy policy</a> and agree to the processing of my data according to the privacy policy.</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Send message</button>
-        <?php
-        if (isset($_POST['name'], $_POST['email'], $_POST['message'], $_POST['privacy-policy'])) {
-            $name = $_POST['name'];
-            $email = $_POST['email'];
-            $message = $_POST['message'];
 
-            $to = "info@onlyplants.com";
-            $subject = "New message from $name";
-            $body = "You have received a new message from $name.\n\n".
-                "Email address: $email\n\n".
-                "Message:\n$message";
+<div class="container mb-5 myCont-">
 
-            // Send email
-            if (mail($to, $subject, $body)) {
-                echo "<p>Thank you for your message. We will contact you soon.</p>";
+    <div class="row align-items-center justify-content-start">
+        <div class="col-lg-2 col-md-10 col-sm-8 col-xs-6 myCol-">
+            <div id="animContainer" class="anim">
+                <script src='https://cdnjs.cloudflare.com/ajax/libs/bodymovin/5.10.1/lottie.min.js'></script> <!-- path of bodymovin library-->
+                <script  src="../js/script2.js"></script>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-10 col-sm-8 col-xs-6 myCol-">
+            <h1>Contact Us!</h1>
+        </div>
+    </div>
+    <div class="form-container shadow p-4">
+        <form action="" method="post">
+            <p>Please read our <a href="privacyPolicy.php">privacy policy</a> before filling out the form.</p>
+            <div class="form-group mb-4">
+                <label for="name">Name:</label>
+                <input type="text" class="form-control my-2" id="name" name="name" required>
+            </div>
+            <div class="form-group mb-4">
+                <label for="email">Email address:</label>
+                <input type="email" class="form-control my-2" id="email" name="email" required>
+            </div>
+            <div class="form-group mb-4">
+                <label class="mb-2" for="message">Message:</label>
+                <textarea class="form-control" id="message" name="message" required></textarea>
+            </div>
+            <div class="form-group mb-4">
+                <input type="checkbox" id="privacy-policy" name="privacy-policy" required>
+                <label for="privacy-policy">I have read the <a href="privacyPolicy.php">privacy policy</a> and agree to the processing of my data according to the privacy policy.</label>
+            </div>
+            <button type="submit" class="btn btn-dark">Send message</button>
+            <?php
+            if (isset($_POST['name'], $_POST['email'], $_POST['message'], $_POST['privacy-policy'])) {
+                $name = $_POST['name'];
+                $email = $_POST['email'];
+                $message = $_POST['message'];
 
-                // Delete user data after successful message send
-                unset($_POST['name']);
-                unset($_POST['email']);
-                unset($_POST['message']);
-                unset($_POST['privacy-policy']);
-            } else {
-                echo "<p>Sorry, there was an error sending the message. Please try again later.</p>";
+                $to = "info@onlyplants.com";
+                $subject = "New message from $name";
+                $body = "You have received a new message from $name.\n\n".
+                    "Email address: $email\n\n".
+                    "Message:\n$message";
+
+                // Send email
+                if (mail($to, $subject, $body)) {
+                    echo "<p>Thank you for your message. We will contact you soon.</p>";
+
+                    // Delete user data after successful message send
+                    unset($_POST['name']);
+                    unset($_POST['email']);
+                    unset($_POST['message']);
+                    unset($_POST['privacy-policy']);
+                } else {
+                    echo "<p>Sorry, there was an error sending the message. Please try again later.</p>";
+                }
             }
-        }
-        ?>
-    </form>
+            ?>
+        </form>
+    </div>
+
 </div>
+
 <?php include "templates/footer.php"?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
